74fe6f292aafb5f4c35e2f1695d48f4b669fd288
Usedtobethere <josesolis2001@hotmail.com>
2022-09-27 10:04:22 -0500

    toggle onClick fixed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 toggle = document.querySelectorAll(".toggle")[0];
+other = document.getElementsByClassName("other");
 nav = document.querySelectorAll("nav")[0];
 toggle_open_text = "Menu";
 toggle_close_text = "Close";
@@ -16,6 +17,21 @@ toggle.addEventListener(
   },
   false
 );
+for (var i = 0; i < other.length; i++) {
+  other[i].addEventListener(
+    "click",
+    function () {
+      nav.classList.toggle("open");
+
+      if (nav.classList.contains("open")) {
+        toggle.innerHTML = toggle_close_text;
+      } else {
+        toggle.innerHTML = toggle_open_text;
+      }
+    },
+    false
+  );
+}
 
 setTimeout(function () {
   nav.classList.toggle("open");
